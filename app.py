9b84f972a53f3bc0ee775f4525161994edf2ba92
garg573 <gauravgarg573@gmail.com>
2022-08-17 17:42:39 +0530

    noapp

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 
-@app.route('',methods=['POST','GET'])
+@app.route('/',methods=['POST','GET'])
 def index():
     if request.method=='POST':
         task_content = request.form['content']
